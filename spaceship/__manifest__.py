7e9597bee8567db7aa4bf5fa1da475c5aedb82b6
Dante Falzone <dante.falzone@deckersports.com>
2023-09-08 14:55:42 -0500

    started creating new XML view for spaceships

--- spaceship/__manifest__.py
+++ spaceship/__manifest__.py
@@ -2,10 +2,14 @@
     'name': "spaceship",
     'version': '1.0',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        "security/ir.model.access.csv",
+        "views/spaceship_views.xml",
+        "views/spaceship_gen_menus.xml"
+    ],
     'author': "Dante Falzone",
     'category': 'sales/Galaxy Sales',
     'description': "A module for selling spaceships! Whee!",
     'installable': True,
-    'application': True,
+    'application': True
 }
--- spaceship/models/spaceship.py
+++ spaceship/models/spaceship.py
@@ -1,8 +1,11 @@
 from odoo import fields, models
 
+
 class Spaceship(models.Model):
-    _name = "space.ship"
+    _name = "spaceship.gen"
     _description = "Generic Spaceship"
 
-
-    spaceship_mfg_model = fields.Char(required=True)
+    spaceship_mfg_model = fields.Char(default="Unknown manufacturer")
+    spaceship_mfg_date = fields.Date()
+    lightspeed_capable = fields.Boolean(required=True)
+    planet_of_origin = fields.Char(default="Earth")
