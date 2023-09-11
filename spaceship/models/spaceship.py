bb337a201234f0d723d5d9271183cd7a8efbef80
Dante Falzone <dante.falzone@deckersports.com>
2023-09-11 14:51:49 -0500

    Added computed field for age of spaceships

--- spaceship/models/spaceship.py
+++ spaceship/models/spaceship.py
@@ -1,12 +1,21 @@
 from odoo import api, fields, models
+from dateutil.relativedelta import relativedelta
+from datetime import date
 
 
 class Spaceship(models.Model):
     _name = "spaceship.gen"
     _description = "Generic Spaceship"
 
-    name = fields.Char(compute="_compute_name", string="Manufacturer and Model")
+    # Since this app is for selling spaceships, possibly lightspeed-capable and offworld-manufactured,
+    # I reckon spaceship ages as if it were 150 years in the future.
+    _canon_present_day: date = fields.Date.today() + relativedelta(years=150)
 
+    name = fields.Char(compute="_compute_name", string="Manufacturer and Model")
+    years_of_age = fields.Char(
+        compute="_compute_spaceship_age",
+        string=f"Years of Age as of {_canon_present_day.strftime('%m/%d/%Y')}"
+    )
     spaceship_mfg = fields.Many2one("spaceship.mfg", string="Spaceship Manufacturer")
     spaceship_mfg_model = fields.Char(default="Unknown model", string="Model")
     spaceship_mfg_date = fields.Date(string="Date of Manufacture")
@@ -16,4 +25,10 @@ class Spaceship(models.Model):
     @api.depends("spaceship_mfg", "spaceship_mfg_model")
     def _compute_name(self):
         for record in self:
-            record.name = f"{self.spaceship_mfg.name} {self.spaceship_mfg_model}"
+            record.name = f"{record.spaceship_mfg.name} {record.spaceship_mfg_model}"
+
+    @api.depends("spaceship_mfg_date")
+    def _compute_spaceship_age(self):
+        for record in self:
+            mfg_date = fields.Date.to_date(record.spaceship_mfg_date)
+            record.years_of_age = str(relativedelta(self._canon_present_day, mfg_date).years)
