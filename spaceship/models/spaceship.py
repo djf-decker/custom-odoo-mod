28fd642963d8182395bd6d0768c4f0e1fbe290a1
Dante Falzone <dante.falzone@deckersports.com>
2023-09-13 11:17:32 -0500

    Added inverse method to update manufacturing year by age

--- spaceship/models/spaceship.py
+++ spaceship/models/spaceship.py
@@ -14,11 +14,13 @@ class Spaceship(models.Model):
     name = fields.Char(compute="_compute_name", string="Manufacturer and Model")
     years_of_age = fields.Char(
         compute="_compute_spaceship_age",
+        inverse="_inverse_spaceship_age",
+        store=True,
         string=f"Years of Age as of {_canon_present_day.strftime('%m/%d/%Y')}"
     )
-    spaceship_mfg = fields.Many2one("spaceship.mfg", string="Spaceship Manufacturer")
+    spaceship_mfg = fields.Many2one("spaceship.mfg", required=True, string="Spaceship Manufacturer")
     spaceship_mfg_model = fields.Char(default="Unknown model", string="Model")
-    spaceship_mfg_date = fields.Date(string="Date of Manufacture")
+    spaceship_mfg_date = fields.Date(required=True, string="Date of Manufacture")
     lightspeed_capable = fields.Boolean(required=True, string="Has Lightspeed Capability")
     planet_of_origin = fields.Char(default="Earth", string="Planet of Origin")
 
@@ -32,3 +34,9 @@ class Spaceship(models.Model):
         for record in self:
             mfg_date = fields.Date.to_date(record.spaceship_mfg_date)
             record.years_of_age = str(relativedelta(self._canon_present_day, mfg_date).years)
+
+    def _inverse_spaceship_age(self):
+        for record in self:
+            current_year = self._canon_present_day.year
+            new_mfg_year = current_year - int(record.years_of_age)
+            record.spaceship_mfg_date = record.spaceship_mfg_date.replace(year=new_mfg_year)
